c37231de3e40ba0bc774a46fca45085cdde39fbd
Jacob Mossberg <jacob.c.mossberg@gmail.com>
2017-02-19 10:51:12 +0100

    [gbgcpp][Tennis Kata] Remove all unit tests except one initial test

--- src/Tennis.hpp
+++ src/Tennis.hpp
@@ -1,19 +1,7 @@
 #include <string>
 
-typedef enum {NORMAL, DEUCE, ADVANTAGE, WINNER} State_T;
-
 class Tennis
 {
 public:
-	Tennis();
-	void winBall(std::string player);
-	int absDiff();
-	int leaderPoints();
-	int pointsAsScore(int points);
-	std::string leader();
-	State_T state();
 	std::string score();
-private:
-	int p1;
-	int p2;
 };
